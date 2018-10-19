107e873b70b5036edacfb19f7271e1cc492d8827
Tao BERQUER <tao.berquer@gmail.com>
2018-10-19 12:03:21 +0200

    Ajout d'un tableau d'utilisateur dans l'administration.

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -14,7 +15,9 @@ class UserController extends Controller
      */
     public function index()
     {
-        return view('admin.user.index');
+        $users = User::all();
+
+        return view('admin.user.index', compact('users'));
     }
 
     /**
--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -3,7 +3,31 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-
+            <table class="table">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nom</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Rôle</th>
+                    <th scope="col">Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($users as $user)
+                    <tr>
+                        <th scope="row">{{$user->id}}</th>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>{{ $user->role }}</td>
+                        <td>
+                            <button type="button" class="btn btn-warning">Modifier</button>
+                            <button type="button" class="btn btn-danger">Supprimer</button>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 @endsection
