6c32ebc34f83aee9a368eeda2bc2de0aa36dbffb
Tao BERQUER <tao.berquer@gmail.com>
2018-10-19 11:14:21 +0200

    Ajout d'un seed pour l'utilisateur 'admin'.

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin'),
+            'role' => 'admin'
+        ]);
+    }
+}
