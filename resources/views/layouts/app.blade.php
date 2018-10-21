0dcf49d13e394eb68670bcfe2ca2e8ca2980d4cd
Tao BERQUER <tao.berquer@gmail.com>
2018-10-21 17:31:34 +0200

    Refonte du dropdown de connexion.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
+    <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
@@ -55,8 +56,12 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('adminUsersHome') }}">Admin - Utilisateurs</a>
-                                    <a class="dropdown-item" href="{{ route('adminParkingsHome') }}">Admin - Parkings</a>
+                                    <a class="dropdown-item" href="#">Mon Profil</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item disabled font-weight-bold text-center">Administration</a>
+                                    <a class="dropdown-item" href="{{ route('adminUsersHome') }}">Utilisateurs</a>
+                                    <a class="dropdown-item" href="{{ route('adminParkingsHome') }}">Parkings</a>
+                                    <div class="dropdown-divider"></div>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
